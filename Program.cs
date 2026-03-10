635f92ffac00b39554f1bd46d776c7c555e45425
Artem Diachuk <s33796@pjwstk.edu.pl>
2026-03-10 17:12:13 +0100

    Diversion commit

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         int[] numbers = { 1, 2, 3, 4, 5 };
-        Console.WriteLine("Project started");
+        Console.WriteLine("Project has started");
         
         Console.WriteLine("Enter your name:");
         string name = Console.ReadLine();
