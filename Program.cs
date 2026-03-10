dcf7da072540da2d5ef9d33e6df594009adaee14
Artem Diachuk <s33796@pjwstk.edu.pl>
2026-03-10 16:48:16 +0100

    Add basic Program structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Project started");
+    }
+}
