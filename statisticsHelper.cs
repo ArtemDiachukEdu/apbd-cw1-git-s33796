d703c873061265f9ed5a862e480ffb854f82d76c
Artem Diachuk <s33796@pjwstk.edu.pl>
2026-03-10 17:00:09 +0100

    Add method CalculateAverage

--- statisticsHelper.cs
+++ statisticsHelper.cs
@@ -2,4 +2,17 @@ namespace MyProject;
 
 public class StatisticsHelper
 {
+    public static double CalculateAverage(int[] values)
+    {
+        double sum = 0;
+        foreach (var v in values) sum += v;
+        return sum / values.Length;
+    }
+    
+    
+    
+    
+    
+    
+    
 }
