a77fe3f147ab4cb828cdc7d8c714095841639eee
Artem Diachuk <s33796@pjwstk.edu.pl>
2026-03-12 21:41:02 +0100

    Changed a line to create a conflict

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ class Program
         double min = StatisticsHelper.CalculateMin(numbers);
         Console.WriteLine("Minimum value is: " + min);
 
-        Console.WriteLine("Hey, this line is for task 6")
+        Console.WriteLine("This is a message from feature-conflict branch")
     }
 }
